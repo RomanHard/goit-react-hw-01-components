a2f92b0b17707ccba255247d5c12e378a59c0ff6
Roman <romeenice@gmail.com>
2023-01-25 20:34:03 +0200

    summary

---
+++ src/App.js
@@ -0,0 +1,14 @@
+export default function App() {
+  const { username, tag, location, avatar, stats } = user;
+  return (
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from './App';
 import './index.css';
 import user from './user.json';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-const data = user[0];
-
-const painting = (
-  <div class="profile">
-    <div class="description">
-      <img src={data.avatar} alt={data.username} class="avatar" />
-      <p class="name">{data.username}</p>
-      <p class="tag">{data.tag}</p>
-      <p class="location">{data.location}</p>
-    </div>
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">{data.stats.followers}</span>
-      </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">{data.stats.views}</span>
-      </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">{data.stats.likes}</span>
-      </li>
-    </ul>
-  </div>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-ReactDOM.render(painting, document.querySelector('#root'));
